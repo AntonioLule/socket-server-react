881ff8eb04ef0894f3ee5e65c5e15ea42aa700bc
antoniolule <antonio_lule@hotmail.com>
2021-06-08 23:09:59 -0500

    nuevos eventos escucha

--- models/sockets.js
+++ models/sockets.js
@@ -21,6 +21,29 @@ class Sockets{
             // Emitir al cliente conectado todas las bandas actuales 
             socket.emit('current-bands', this.bandList.getBands() );
 
+            // votar por la banda
+            socket.on( 'votar-banda', (id) => {
+                this.bandList.increaseVotes(id);
+                this.io.emit('current-bands', this.bandList.getBands() );
+            })
+
+            // borrar banda  
+            socket.on( 'borrar-banda', (id) => {
+                this.bandList.removeBand(id);
+                this.io.emit('current-bands', this.bandList.getBands() );
+            })
+
+            // cambiar-nombre-banda
+            socket.on( 'cambiar-nombre-banda', ({id,nombre}) => {
+                this.bandList.changeName(id,nombre);
+                this.io.emit('current-bands', this.bandList.getBands() );
+            })
+
+            //nueva-banda
+            socket.on( 'nueva-banda', ({nombre}) => {
+                this.bandList.addband(nombre);
+                this.io.emit('current-bands', this.bandList.getBands() );
+            })
 
         });
 
